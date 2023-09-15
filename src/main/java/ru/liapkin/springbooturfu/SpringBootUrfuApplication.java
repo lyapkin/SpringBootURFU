f9b18f795662ec36773fb9269d1223ac9de28779
lyapkin <lyapkin992@gmail.com>
2023-09-15 15:20:16 +0500

    init

---
+++ src/main/java/ru/liapkin/springbooturfu/SpringBootUrfuApplication.java
@@ -0,0 +1,13 @@
+package ru.liapkin.springbooturfu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUrfuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUrfuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/liapkin/springbooturfu/SpringBootUrfuApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.liapkin.springbooturfu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUrfuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
